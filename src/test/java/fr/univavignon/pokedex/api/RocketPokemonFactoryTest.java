386fe7f3ed4d49cae83f1edf1360640d8b430d05
anthony-navarro <91269417+anthony-navarro@users.noreply.github.com>
2023-05-13 02:41:08 +0200

    feat: test Rocket PokemonFactory with PokemonFactory tests

--- src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
@@ -5,17 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+public class RocketPokemonFactoryTest {
 
-class IPokemonFactoryTest
-{
-    IPokemonFactory pokemonFactory = new PokemonFactory();
+    IPokemonFactory rocketPokemonFactory = new RocketPokemonFactory();
     Pokemon charmander;
     Pokemon charmanderFactory;
 
     @BeforeEach
     public void setup() throws PokedexException {
         charmander = new Pokemon(2,"Salamèche",203,185,229,219,282,2500,6,100);
-        charmanderFactory = pokemonFactory.createPokemon(2, 219,282,2500,6);
+        charmanderFactory = rocketPokemonFactory.createPokemon(2, 219,282,2500,6);
     }
     @Test
     public void shouldCreatePokemon()
@@ -29,13 +28,13 @@ class IPokemonFactoryTest
         assertEquals(charmander.getHp(), charmanderFactory.getHp());
         assertEquals(charmander.getDust(), charmanderFactory.getDust());
         assertEquals(charmander.getCandy(), charmanderFactory.getCandy());
-        assertEquals(100, charmander.getIv());
+        assertEquals(charmander.getIv(), charmanderFactory.getIv());
     }
 
     @Test
     public void shouldThrowWhenIndexTooLowOrHigh()
     {
-        assertThrows(PokedexException.class, () -> pokemonFactory.createPokemon(-1,219,282,2500,6));
-        assertThrows(PokedexException.class, () -> pokemonFactory.createPokemon(151,219,282,2500,6));
+        assertThrows(PokedexException.class, () -> rocketPokemonFactory.createPokemon(-1,219,282,2500,6));
+        assertThrows(PokedexException.class, () -> rocketPokemonFactory.createPokemon(151,219,282,2500,6));
     }
 }
